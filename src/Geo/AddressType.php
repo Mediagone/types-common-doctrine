4b4c50801e951681a968b15b3e969af2eb335eb7
Bruce <bruce@mediagone.fr>
2021-03-10 01:10:46 +0100

    New "AddressType" Doctrine type

---
+++ src/Geo/AddressType.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+
+namespace Mediagone\Doctrine\Types\Common\Geo;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+use Mediagone\Types\Common\Geo\Address;
+
+
+final class AddressType extends Type
+{
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+    
+    public const NAME = 'common_address';
+    
+    
+    
+    //========================================================================================================
+    // Type implementation
+    //========================================================================================================
+    
+    public function getName() : string
+    {
+        return self::NAME;
+    }
+    
+    
+    /**
+     * Gets the SQL declaration snippet for a field of this type.
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform) : string
+    {
+        return $platform->getVarcharTypeDeclarationSQL([
+            'length' => Address::MAX_LENGTH,
+        ]);
+    }
+    
+    
+    /**
+     * Adds an SQL comment to typehint the actual Doctrine Type for reverse schema engineering.
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform) : bool
+    {
+        return true;
+    }
+    
+    
+    /**
+     * Converts a value from its database representation to its PHP representation of this type.
+     *
+     * @param string|null $value The value to convert.
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform) : ?Address
+    {
+        return $value !== null ? Address::fromString($value) : null;
+    }
+    
+    
+    /**
+     * Converts a value from its PHP representation to its database representation of this type.
+     *
+     * @param Address|null $value The value to convert.
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform) : ?string
+    {
+        return $value ? (string)$value : null;
+    }
+    
+    
+    
+}
---
+++ tests/Geo/AddressTypeTest.php
@@ -0,0 +1,90 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Mediagone\Doctrine\Types\Common\Geo;
+
+use Doctrine\DBAL\Platforms\MySqlPlatform;
+use Doctrine\DBAL\Platforms\PostgreSql94Platform;
+use Doctrine\DBAL\Types\Type;
+use Mediagone\Doctrine\Types\Common\Geo\AddressType;
+use Mediagone\Types\Common\Geo\Address;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * @covers \Mediagone\Doctrine\Types\Common\Geo\AddressType
+ */
+final class AddressTypeTest extends TestCase
+{
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+    
+    private AddressType $type;
+    
+    
+    
+    //========================================================================================================
+    // Initialization
+    //========================================================================================================
+    
+    public static function setUpBeforeClass() : void
+    {
+        if (!Type::hasType(AddressType::NAME)) {
+            Type::addType(AddressType::NAME, AddressType::class);
+        }
+    }
+    
+    
+    public function setUp() : void
+    {
+        $this->type = Type::getType(AddressType::NAME);
+    }
+    
+    
+    
+    //========================================================================================================
+    // Tests
+    //========================================================================================================
+    
+    public function test_can_return_its_name() : void
+    {
+        self::assertSame(AddressType::NAME, $this->type->getName());
+    }
+    
+    
+    public function test_requires_comment_hint() : void
+    {
+        self::assertTrue($this->type->requiresSQLCommentHint(new MySqlPlatform()));
+        self::assertTrue($this->type->requiresSQLCommentHint(new PostgreSql94Platform()));
+    }
+    
+    
+    public function test_declare_sql() : void
+    {
+        self::assertSame('VARCHAR('.Address::MAX_LENGTH.')', $this->type->getSQLDeclaration([], new MySqlPlatform()));
+        self::assertSame('VARCHAR('.Address::MAX_LENGTH.')', $this->type->getSQLDeclaration(['length' => '70'], new MySqlPlatform()));
+        self::assertSame('VARCHAR('.Address::MAX_LENGTH.')', $this->type->getSQLDeclaration(['length' => '200'], new MySqlPlatform()));
+    }
+    
+    
+    public function test_can_convert_to_database_value() : void
+    {
+        $address = Address::fromString('Test address');
+        $value = $this->type->convertToDatabaseValue($address, new MySqlPlatform());
+        
+        self::assertSame($value, (string)$address);
+    }
+    
+    
+    public function test_can_convert_value_from_database() : void
+    {
+        $value = 'Test address';
+        $address = $this->type->convertToPHPValue($value, new MySqlPlatform());
+        
+        self::assertInstanceOf(Address::class, $address);
+        self::assertSame($value, (string)$address);
+    }
+    
+    
+    
+}
