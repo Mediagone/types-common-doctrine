b5811ae7db463d04d57ff14fcdb39d0aecf94902
Bruce <bruce@mediagone.fr>
2021-03-10 01:07:43 +0100

    New "CityType" Doctrine type

---
+++ src/Geo/CityType.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+
+namespace Mediagone\Doctrine\Types\Common\Geo;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+use Mediagone\Types\Common\Geo\City;
+
+
+final class CityType extends Type
+{
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+    
+    public const NAME = 'common_city';
+    
+    
+    
+    //========================================================================================================
+    // Type implementation
+    //========================================================================================================
+    
+    public function getName() : string
+    {
+        return self::NAME;
+    }
+    
+    
+    /**
+     * Gets the SQL declaration snippet for a field of this type.
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform) : string
+    {
+        return $platform->getVarcharTypeDeclarationSQL([
+            'length' => City::MAX_LENGTH,
+        ]);
+    }
+    
+    
+    /**
+     * Adds an SQL comment to typehint the actual Doctrine Type for reverse schema engineering.
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform) : bool
+    {
+        return true;
+    }
+    
+    
+    /**
+     * Converts a value from its database representation to its PHP representation of this type.
+     *
+     * @param string|null $value The value to convert.
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform) : ?City
+    {
+        return $value !== null ? City::fromString($value) : null;
+    }
+    
+    
+    /**
+     * Converts a value from its PHP representation to its database representation of this type.
+     *
+     * @param City|null $value The value to convert.
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform) : ?string
+    {
+        return $value ? (string)$value : null;
+    }
+    
+    
+    
+}
---
+++ tests/Geo/CityTypeTest.php
@@ -0,0 +1,90 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Mediagone\Doctrine\Types\Common\Geo;
+
+use Doctrine\DBAL\Platforms\MySqlPlatform;
+use Doctrine\DBAL\Platforms\PostgreSql94Platform;
+use Doctrine\DBAL\Types\Type;
+use Mediagone\Doctrine\Types\Common\Geo\CityType;
+use Mediagone\Types\Common\Geo\City;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * @covers \Mediagone\Doctrine\Types\Common\Geo\CityType
+ */
+final class CityTypeTest extends TestCase
+{
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+    
+    private CityType $type;
+    
+    
+    
+    //========================================================================================================
+    // Initialization
+    //========================================================================================================
+    
+    public static function setUpBeforeClass() : void
+    {
+        if (!Type::hasType(CityType::NAME)) {
+            Type::addType(CityType::NAME, CityType::class);
+        }
+    }
+    
+    
+    public function setUp() : void
+    {
+        $this->type = Type::getType(CityType::NAME);
+    }
+    
+    
+    
+    //========================================================================================================
+    // Tests
+    //========================================================================================================
+    
+    public function test_can_return_its_name() : void
+    {
+        self::assertSame(CityType::NAME, $this->type->getName());
+    }
+    
+    
+    public function test_requires_comment_hint() : void
+    {
+        self::assertTrue($this->type->requiresSQLCommentHint(new MySqlPlatform()));
+        self::assertTrue($this->type->requiresSQLCommentHint(new PostgreSql94Platform()));
+    }
+    
+    
+    public function test_declare_sql() : void
+    {
+        self::assertSame('VARCHAR('.City::MAX_LENGTH.')', $this->type->getSQLDeclaration([], new MySqlPlatform()));
+        self::assertSame('VARCHAR('.City::MAX_LENGTH.')', $this->type->getSQLDeclaration(['length' => '70'], new MySqlPlatform()));
+        self::assertSame('VARCHAR('.City::MAX_LENGTH.')', $this->type->getSQLDeclaration(['length' => '200'], new MySqlPlatform()));
+    }
+    
+    
+    public function test_can_convert_to_database_value() : void
+    {
+        $city = City::fromString('Test city');
+        $value = $this->type->convertToDatabaseValue($city, new MySqlPlatform());
+        
+        self::assertSame($value, (string)$city);
+    }
+    
+    
+    public function test_can_convert_value_from_database() : void
+    {
+        $value = 'Test city';
+        $city = $this->type->convertToPHPValue($value, new MySqlPlatform());
+        
+        self::assertInstanceOf(City::class, $city);
+        self::assertSame($value, (string)$city);
+    }
+    
+    
+    
+}
