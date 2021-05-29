03219fcc878b239d587a00d8c6c4014c540ad7c1
Bruce <bruce@mediagone.fr>
2021-05-29 10:29:45 +0200

    New "Quantity" Doctrine type

---
+++ src/System/QuantityType.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+
+namespace Mediagone\Doctrine\Types\Common\System;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+use Mediagone\Types\Common\System\Quantity;
+
+
+final class QuantityType extends Type
+{
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+    
+    public const NAME = 'common_quantity';
+    
+    
+    
+    //========================================================================================================
+    // Type implementation
+    //========================================================================================================
+    
+    public function getName() : string
+    {
+        return self::NAME;
+    }
+    
+    
+    /**
+     * Gets the SQL declaration snippet for a field of this type.
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform) : string
+    {
+        return $platform->getIntegerTypeDeclarationSQL([
+        ]);
+    }
+    
+    
+    /**
+     * Adds an SQL comment to typehint the actual Doctrine Type for reverse schema engineering.
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform) : bool
+    {
+        return true;
+    }
+    
+    
+    /**
+     * Converts a value from its database representation to its PHP representation of this type.
+     *
+     * @param int|null $value The value to convert.
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform) : ?Quantity
+    {
+        return $value !== null ? Quantity::fromInt((int)$value) : null;
+    }
+    
+    
+    /**
+     * Converts a value from its PHP representation to its database representation of this type.
+     *
+     * @param Quantity|null $value The value to convert.
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform) : ?int
+    {
+        return $value ? $value->toInteger() : null;
+    }
+    
+    
+    
+}
---
+++ tests/System/QuantityTypeTest.php
@@ -0,0 +1,88 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Mediagone\Doctrine\Types\Common\System;
+
+use Doctrine\DBAL\Platforms\MySqlPlatform;
+use Doctrine\DBAL\Platforms\PostgreSQL94Platform;
+use Doctrine\DBAL\Types\Type;
+use Mediagone\Types\Common\System\Quantity;
+use Mediagone\Doctrine\Types\Common\System\QuantityType;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * @covers \Mediagone\Doctrine\Types\Common\System\QuantityType
+ */
+final class QuantityTypeTest extends TestCase
+{
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+    
+    private QuantityType $type;
+    
+    
+    
+    //========================================================================================================
+    // Initialization
+    //========================================================================================================
+    
+    public static function setUpBeforeClass() : void
+    {
+        if (!Type::hasType(QuantityType::NAME)) {
+            Type::addType(QuantityType::NAME, QuantityType::class);
+        }
+    }
+    
+    
+    public function setUp() : void
+    {
+        $this->type = Type::getType(QuantityType::NAME);
+    }
+    
+    
+    
+    //========================================================================================================
+    // Tests
+    //========================================================================================================
+    
+    public function test_can_return_its_name() : void
+    {
+        self::assertSame(QuantityType::NAME, $this->type->getName());
+    }
+    
+    
+    public function test_requires_comment_hint() : void
+    {
+        self::assertTrue($this->type->requiresSQLCommentHint(new MySqlPlatform()));
+        self::assertTrue($this->type->requiresSQLCommentHint(new PostgreSql94Platform()));
+    }
+    
+    
+    public function test_declare_sql() : void
+    {
+        self::assertSame('INT', $this->type->getSQLDeclaration([], new MySqlPlatform()));
+        self::assertSame('INT', $this->type->getSQLDeclaration(['length' => '70'], new MySqlPlatform()));
+        self::assertSame('INT', $this->type->getSQLDeclaration(['length' => '200'], new MySqlPlatform()));
+    }
+    
+    
+    public function test_can_convert_to_database_value() : void
+    {
+        $value = $this->type->convertToDatabaseValue(Quantity::fromInt(10), new MySqlPlatform());
+        
+        self::assertSame(10, $value);
+    }
+    
+    
+    public function test_can_convert_value_from_database() : void
+    {
+        $value = $this->type->convertToPHPValue(10, new MySqlPlatform());
+        
+        self::assertInstanceOf(Quantity::class, $value);
+        self::assertSame(10, $value->toInteger());
+    }
+    
+    
+    
+}
