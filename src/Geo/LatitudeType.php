96824bc18ee3132cf89e3d570d133c4952698df9
Bruce <bruce@mediagone.fr>
2021-08-26 12:58:57 +0200

    New "LatitudeType" Doctrine type

---
+++ src/Geo/LatitudeType.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace Mediagone\Doctrine\Types\Common\Geo;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+use Mediagone\Types\Common\Geo\Latitude;
+
+
+final class LatitudeType extends Type
+{
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+    
+    public const NAME = 'common_latitude';
+    
+    
+    
+    //========================================================================================================
+    // Type implementation
+    //========================================================================================================
+    
+    public function getName() : string
+    {
+        return self::NAME;
+    }
+    
+    
+    /**
+     * Gets the SQL declaration snippet for a field of this type.
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform) : string
+    {
+        return $platform->getFloatDeclarationSQL([]);
+    }
+    
+    
+    /**
+     * Adds an SQL comment to typehint the actual Doctrine Type for reverse schema engineering.
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform) : bool
+    {
+        return true;
+    }
+    
+    
+    /**
+     * Converts a value from its database representation to its PHP representation of this type.
+     *
+     * @param float|null $value The value to convert.
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform) : ?Latitude
+    {
+        return $value !== null ? Latitude::fromFloat($value) : null;
+    }
+    
+    
+    /**
+     * Converts a value from its PHP representation to its database representation of this type.
+     *
+     * @param Latitude|null $value The value to convert.
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform) : ?float
+    {
+        return $value ? $value->toFloat() : null;
+    }
+    
+    
+    
+}
---
+++ tests/Geo/LatitudeTypeTest.php
@@ -0,0 +1,90 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Mediagone\Doctrine\Types\Common\Geo;
+
+use Doctrine\DBAL\Platforms\MySqlPlatform;
+use Doctrine\DBAL\Platforms\PostgreSql94Platform;
+use Doctrine\DBAL\Types\Type;
+use Mediagone\Doctrine\Types\Common\Geo\LatitudeType;
+use Mediagone\Types\Common\Geo\Latitude;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * @covers \Mediagone\Doctrine\Types\Common\Geo\LatitudeType
+ */
+final class LatitudeTypeTest extends TestCase
+{
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+    
+    private LatitudeType $type;
+    
+    
+    
+    //========================================================================================================
+    // Initialization
+    //========================================================================================================
+    
+    public static function setUpBeforeClass() : void
+    {
+        if (!Type::hasType(LatitudeType::NAME)) {
+            Type::addType(LatitudeType::NAME, LatitudeType::class);
+        }
+    }
+    
+    
+    public function setUp() : void
+    {
+        $this->type = Type::getType(LatitudeType::NAME);
+    }
+    
+    
+    
+    //========================================================================================================
+    // Tests
+    //========================================================================================================
+    
+    public function test_can_return_its_name() : void
+    {
+        self::assertSame(LatitudeType::NAME, $this->type->getName());
+    }
+    
+    
+    public function test_requires_comment_hint() : void
+    {
+        self::assertTrue($this->type->requiresSQLCommentHint(new MySqlPlatform()));
+        self::assertTrue($this->type->requiresSQLCommentHint(new PostgreSql94Platform()));
+    }
+    
+    
+    public function test_declare_sql() : void
+    {
+        self::assertSame('DOUBLE PRECISION', $this->type->getSQLDeclaration([], new MySqlPlatform()));
+        self::assertSame('DOUBLE PRECISION', $this->type->getSQLDeclaration(['scale' => '6'], new MySqlPlatform()));
+        self::assertSame('DOUBLE PRECISION', $this->type->getSQLDeclaration(['precision' => '10'], new MySqlPlatform()));
+    }
+    
+    
+    public function test_can_convert_to_database_value() : void
+    {
+        $value = 20.123456;
+        $latitude = $this->type->convertToDatabaseValue(Latitude::fromFloat($value), new MySqlPlatform());
+        
+        self::assertSame($value, $latitude);
+    }
+    
+    
+    public function test_can_convert_value_from_database() : void
+    {
+        $value = 20.123456;
+        $latitude = $this->type->convertToPHPValue($value, new MySqlPlatform());
+        
+        self::assertInstanceOf(Latitude::class, $latitude);
+        self::assertSame($value, $latitude->toFloat());
+    }
+    
+    
+    
+}
