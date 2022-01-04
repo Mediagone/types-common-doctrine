2bb901dce6eaf3d1560dea0fd845c0b4c62c7c26
Bruce <bruce@mediagone.fr>
2022-01-04 19:56:53 +0100

    New "Iban" Doctrine type

---
+++ src/Business/IbanType.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+
+namespace Mediagone\Doctrine\Types\Common\Business;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+use Mediagone\Types\Common\Business\Iban;
+
+
+final class IbanType extends Type
+{
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+    
+    public const NAME = 'common_iban';
+    
+    
+    
+    //========================================================================================================
+    // Type implementation
+    //========================================================================================================
+    
+    public function getName() : string
+    {
+        return self::NAME;
+    }
+    
+    
+    /**
+     * Gets the SQL declaration snippet for a field of this type.
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform) : string
+    {
+        return $platform->getVarcharTypeDeclarationSQL([
+            'length' => Iban::MAX_LENGTH,
+        ]);
+    }
+    
+    
+    /**
+     * Adds an SQL comment to typehint the actual Doctrine Type for reverse schema engineering.
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform) : bool
+    {
+        return true;
+    }
+    
+    
+    /**
+     * Converts a value from its database representation to its PHP representation of this type.
+     *
+     * @param string|null $value The value to convert.
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform) : ?Iban
+    {
+        return $value !== null ? Iban::fromString($value) : null;
+    }
+    
+    
+    /**
+     * Converts a value from its PHP representation to its database representation of this type.
+     *
+     * @param Iban|null $value The value to convert.
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform) : ?string
+    {
+        return $value ? (string)$value : null;
+    }
+    
+    
+    
+}
---
+++ tests/Business/IbanTypeTest.php
@@ -0,0 +1,90 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Mediagone\Doctrine\Types\Common\Business;
+
+use Doctrine\DBAL\Platforms\MySqlPlatform;
+use Doctrine\DBAL\Platforms\PostgreSql94Platform;
+use Doctrine\DBAL\Types\Type;
+use Mediagone\Types\Common\Business\Iban;
+use Mediagone\Doctrine\Types\Common\Business\IbanType;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * @covers \Mediagone\Doctrine\Types\Common\Business\IbanType
+ */
+final class IbanTypeTest extends TestCase
+{
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+    
+    private IbanType $type;
+    
+    
+    
+    //========================================================================================================
+    // Initialization
+    //========================================================================================================
+    
+    public static function setUpBeforeClass() : void
+    {
+        if (!Type::hasType(IbanType::NAME)) {
+            Type::addType(IbanType::NAME, IbanType::class);
+        }
+    }
+    
+    
+    public function setUp() : void
+    {
+        $this->type = Type::getType(IbanType::NAME);
+    }
+    
+    
+    
+    //========================================================================================================
+    // Tests
+    //========================================================================================================
+    
+    public function test_can_return_its_name() : void
+    {
+        self::assertSame(IbanType::NAME, $this->type->getName());
+    }
+    
+    
+    public function test_requires_comment_hint() : void
+    {
+        self::assertTrue($this->type->requiresSQLCommentHint(new MySqlPlatform()));
+        self::assertTrue($this->type->requiresSQLCommentHint(new PostgreSql94Platform()));
+    }
+    
+    
+    public function test_declare_sql() : void
+    {
+        self::assertSame('VARCHAR('.Iban::MAX_LENGTH.')', $this->type->getSQLDeclaration([], new MySqlPlatform()));
+        self::assertSame('VARCHAR('.Iban::MAX_LENGTH.')', $this->type->getSQLDeclaration(['length' => '70'], new MySqlPlatform()));
+        self::assertSame('VARCHAR('.Iban::MAX_LENGTH.')', $this->type->getSQLDeclaration(['length' => '200'], new MySqlPlatform()));
+    }
+    
+    
+    public function test_can_convert_to_database_value() : void
+    {
+        $name = Iban::fromString('CH9300762011623852957');
+        $value = $this->type->convertToDatabaseValue($name, new MySqlPlatform());
+        
+        self::assertSame($value, (string)$name);
+    }
+    
+    
+    public function test_can_convert_value_from_database() : void
+    {
+        $value = 'CH9300762011623852957';
+        $name = $this->type->convertToPHPValue($value, new MySqlPlatform());
+        
+        self::assertInstanceOf(Iban::class, $name);
+        self::assertSame($value, (string)$name);
+    }
+    
+    
+    
+}
